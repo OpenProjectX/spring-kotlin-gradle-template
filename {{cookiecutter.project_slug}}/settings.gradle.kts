0881f1d3227467a0e5d9b3c21a6dac8a5907dea5
CoderYellow <CoderYellow@hotmail.com>
2026-05-23 15:58:52 +0800

    feat: add custom Maven mirrors for non-CI environments in Gradle settings

--- {{cookiecutter.project_slug}}/settings.gradle.kts
+++ {{cookiecutter.project_slug}}/settings.gradle.kts
@@ -2,6 +2,15 @@ import java.io.File
 
 pluginManagement {
     repositories {
+        val isCi = System.getenv().containsKey("CI") ||
+                System.getenv().containsKey("GITHUB_ACTIONS") ||
+                System.getenv().containsKey("JENKINS_HOME")
+
+        if (!isCi) {
+            maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public/")
+            maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+        }
+
         gradlePluginPortal()
         maven(url = "https://repo.spring.io/plugins-release")
     }
@@ -9,6 +18,15 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        val isCi = System.getenv().containsKey("CI") ||
+                System.getenv().containsKey("GITHUB_ACTIONS") ||
+                System.getenv().containsKey("JENKINS_HOME")
+
+        if (!isCi) {
+            maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public/")
+            maven(url = "https://maven.aliyun.com/repository/public/")
+        }
+
         mavenCentral()
     }
 }
